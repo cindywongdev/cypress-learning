fc83302fc8e7b698b791ccd1db42bae4e8f1519e
Cindy Wong <cindywongdev@gmail.com>
2024-02-07 14:54:56 -0500

    edited first test

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,10 @@
 describe ('Fundamentals test', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/fundamentals')
+    // can pass string or regex into methods, and this one specifically makes it so that test is case insensitive
+    // method #1:
+    // cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+    // method #2:
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
 })
