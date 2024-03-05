d37c66b124ae307b6320023588951d0848e48180
Cindy Wong <cindywongdev@gmail.com>
2024-03-05 15:43:33 +0100

    finished unhappy path #1 test and started #2'

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -21,6 +21,18 @@ describe('form tests', () => {
         cy.wait(3000)
         // check that success message has disappeared
         cy.contains(/Successfully subbed: cindywongdev@gmail.com!/i).should('not.exist')
+
+        // #2: unhappy path for invalid email
+        cy.get('@subscribe-input').type('invalid-email') // grab input & type an invalid email
+        cy.contains(/Invalid email: invalid-email!/i).should('not.exist')// check that error msg doesn't exist before clicking
+        cy.getDataTest('subscribe-button').click() // click subscribe
+        cy.contains(/Invalid email: invalid-email!/i).should('exist') // check for error msg
+        cy.wait(3000) // wait for 3s
+        cy.contains(/Invalid email: invalid-email!/i).should('not.exist') // check that error msg is gone
+
+        // #3: unhappy path for empty input
+        // cy.get('@subscribe-input').type('') // this will throw a cypress error bc you cant type nothing, this basically tells cypress to do nothing. so just skip this step and click the button
+        cy.getDataTest('subscribe-button').click()
     })
 })
 
