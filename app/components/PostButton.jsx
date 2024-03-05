c822627ed31200f00aef3b4dea63f5f5366cc0a2
Cindy Wong <cindywongdev@gmail.com>
2024-03-05 18:34:12 +0100

    examples page multi page tests completed

--- app/components/PostButton.jsx
+++ app/components/PostButton.jsx
@@ -5,8 +5,10 @@ import { Button } from "@mui/material"
 export default function PostButton(){
 
     function handleClick(){
-        fetch('http://localhost:3000/examples', {method: 'POST'}).then((data) => {
-            console.log({data})
+        fetch('http://localhost:3000/examples', {method: 'POST'}).then((res) => {
+            // post request returns a promise that resolves as a JSON object
+            // then we gotta turn that response (res) BACK into a javascript object using the json() method, which returns another promise that resolves as a javsascript object
+            res.json().then((data) => console.log(data))
         }).catch(() => {
             console.log('An error occured')
         })
--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,5 +1,28 @@
 describe('various examples', () => {
+    beforeEach(() => {
+        cy.visit('/examples')
+    })
     it('multi-page testing', () => {
-        cy.visit('/').pause()
+        cy.getDataTest('nav-why-cypress').click() // visit home page by clicking button
+        cy.location('pathname').should('equal', '/') // assert that current path is correct
+
+        // do the same for all the other pages! make sure buttons lead to where they r supposed to :)
+        cy.getDataTest('nav-overview').click()
+        cy.location('pathname').should('equal', '/overview')
+
+        cy.getDataTest('nav-fundamentals').click()
+        cy.location('pathname').should('equal', '/fundamentals')
+
+        cy.getDataTest('nav-forms').click()
+        cy.location('pathname').should('equal', '/forms')
+
+        cy.getDataTest('nav-examples').click()
+        cy.location('pathname').should('equal', '/examples')
+
+        cy.getDataTest('nav-component').click()
+        cy.location('pathname').should('equal', '/component')
+
+        cy.getDataTest('nav-best-practices').click()
+        cy.location('pathname').should('equal', '/best-practices')
     })
 })
